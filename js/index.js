584f20b229d53a8d94f9bf055146cc5c61884c41
ayorindeaina <rolandaina22@gmail.com>
2024-12-13 11:48:17 +0100

    restructure folder

--- js/index.js
+++ js/index.js
@@ -1,58 +1,25 @@
-const storeproducts = [
-    {
-        id: 1,
-        imgPath: "../assets/k.png",
-        description: "Loveseat sofa",
-        price: "$24.99"
-    },
-    {
-        id: 2,
-        imgPath: "../assets/y.png",
-        description: "Table lamp",
-        price: "$24.55"
-    },
-    {
-        id: 3,
-        imgPath: "../assets/q.png",
-        description: "Beige table lamp",
-        price: "$24.99"
-    },
-    {
-        id: 4,
-        imgPath: "../assets/w.png",
-        description: "Bamboo basket",
-        price: "$30,67"
-    },
-]
-let containerElem = document.getElementById("new");
+const sliderElem = document.querySelector(".slider");
 
-// mapping through an array
-storeproducts.map((product) => {
-    containerElem.innerHTML += `
-      <div class="card">
-                <img src="${product.imgPath}" alt="" class="card-img">
-                <h4 class="card-description">${product.description}</h4> 
+const cardWidth = 310;
 
-                <p class="card-price">${product.price}</p>
+let currentPosition = 0;
 
-            </div>`
-})
+function slideLeft() {
+    currentPosition += cardWidth;  // currentPosition = currentPosition + cardwidth
+    if (currentPosition > 0) {
+        currentPosition = 0;
+    }
+    updateSliderPosition();
+}
+function slideRight() {
+    const maxPosition = sliderElem.scrollWidth - sliderElem.clientWidth;
+    currentPosition -= cardWidth;  // currentPosition = currentPosition + cardwidth
+    if (currentPosition < -maxPosition) {
+        currentPosition = -maxPosition;
+    }
+    updateSliderPosition();
+}
 
-
-const names = ["caleb", "great", "roland", "mmesoma", "wizzy", "nazo", "divine", "james", "john", "king"];
-names[0] = "joshua";
-names.push("queen","king");
-names.pop();
-names.unshift("sam","happy");
-names.shift();
-names.reverse();
-let selectedNames = names.slice(1,4);
-
-const ulElem = document.getElementById("student-list");
-
-selectedNames.map((item) => {
-    ulElem.innerHTML += `
-    <li> ${item} </li>`
-})
-
-const footballclubs = ["chelsea","liverpool","madrid"]
+function updateSliderPosition() {
+    sliderElem.style.transform = `translate(${currentPosition}px)`;
+}
